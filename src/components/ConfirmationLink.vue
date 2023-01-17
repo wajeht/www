0e93aa5a551a76225f68b4f8b25a8ea4a08f391a
wajeht <58354193+wajeht@users.noreply.github.com>
2023-01-16 20:58:01 -0600

    feat: added nav link confirmation for instagram

---
+++ src/components/ConfirmationLink.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const showModal = ref(false);
+
+const props = defineProps<{
+  content: string;
+  label: string;
+  link: string;
+}>();
+
+function go(): void {
+  window.open(props.link);
+}
+</script>
+
+<template>
+  <span>
+    <p
+      @click="showModal = true"
+      class="hover:underline grayscale hover:grayscale-0 hover:text-[color:#FBF0DF] flex-none cursor-pointer"
+    >
+      {{ label }}
+    </p>
+
+    <slot @click="showModal = true"></slot>
+
+    <Teleport to="body">
+      <div v-if="showModal" @click="$emit('open')" class="relative z-10" role="dialog">
+        <!-- background -->
+        <div class="fixed inset-0 z-10 flex items-center justify-center p-4 bg-black bg-opacity-75">
+          <!-- content -->
+          <div class="flex max-w-3xl flex-col gap-2 px-4 pt-5 pb-4 bg-[color:#252420] rounded-sm">
+            <!-- title -->
+            <div class="flex flex-col gap-4 justify-between text-[color:#FBF0DF]">
+              <p>{{ content }}</p>
+
+              <!-- buttons -->
+              <div class="flex justify-between gap-2">
+                <!-- go ahead -->
+                <button
+                  @click="go()"
+                  class="hover:outline hover:outline-[color:#37362F] flex gap-1 items-center py-1 px-4 rounded-sm font-light hover:bg-[color:#0B0A08] bg-[color:#15140E] text-[color:#D4D3D2] hover:text-[color:#FBF0DF]"
+                >
+                  <small class="text-xs"> ✅ </small>
+                  ok
+                </button>
+
+                <!-- cancel -->
+                <button
+                  @click="showModal = false"
+                  class="hover:outline hover:outline-[color:#37362F] flex gap-1 items-center py-1 px-4 rounded-sm font-light hover:bg-[color:#0B0A08] bg-[color:#15140E] text-[color:#D4D3D2] hover:text-[color:#FBF0DF]"
+                >
+                  <small class="text-xs"> ❌ </small>
+                  close
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Teleport>
+  </span>
+</template>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -3,37 +3,58 @@ const route = useRoute();
 </script>
 
 <template>
-    <div class="flex justify-center lg:justify-end ">
-        <nav class="flex flex-wrap lg:flex-col lg:gap-3 gap-3 font-light text-[color:#D4D3D2] ">
-            <span class="flex gap-3 sm:flex-row lg:flex-col lg:gap-0">
-                <NuxtLink to="/projects"
-                    class="hover:underline grayscale hover:grayscale-0 hover:text-[color:#FBF0DF] flex-none" :class="{
-                        'font-bold text-[color:#FBF0DF] grayscale-0': route.path === '/projects',
-                        'grayscale': route.path !== '/projects'
-                    }">projects 📚
-                </NuxtLink>
-                <NuxtLink to="/contact"
-                    class="hover:underline grayscale hover:grayscale-0 hover:text-[color:#FBF0DF] flex-none" :class="{
-                        'font-bold text-[color:#FBF0DF] grayscale-0': route.path === '/contact',
-                        'grayscale': route.path !== '/contact'
-                    }">contact 💌
-                </NuxtLink>
-                <NuxtLink to="/"
-                    class="hover:underline grayscale hover:grayscale-0 hover:text-[color:#FBF0DF] flex-none" :class="{
-                        'font-bold text-[color:#FBF0DF] grayscale-0': route.path === '/',
-                        'grayscale': route.path !== '/'
-                    }">
-                    about 👨‍💻‍</NuxtLink>
-            </span>
+  <div class="flex justify-center lg:justify-end">
+    <nav class="flex flex-wrap lg:flex-col lg:gap-3 gap-3 font-light text-[color:#D4D3D2]">
+      <span class="flex gap-3 sm:flex-row lg:flex-col lg:gap-0">
+        <NuxtLink
+          to="/projects"
+          class="hover:underline grayscale hover:grayscale-0 hover:text-[color:#FBF0DF] flex-none"
+          :class="{
+            'font-bold text-[color:#FBF0DF] grayscale-0': route.path === '/projects',
+            grayscale: route.path !== '/projects',
+          }"
+          >projects 📚
+        </NuxtLink>
+        <NuxtLink
+          to="/contact"
+          class="hover:underline grayscale hover:grayscale-0 hover:text-[color:#FBF0DF] flex-none"
+          :class="{
+            'font-bold text-[color:#FBF0DF] grayscale-0': route.path === '/contact',
+            grayscale: route.path !== '/contact',
+          }"
+          >contact 💌
+        </NuxtLink>
+        <NuxtLink
+          to="/"
+          class="hover:underline grayscale hover:grayscale-0 hover:text-[color:#FBF0DF] flex-none"
+          :class="{
+            'font-bold text-[color:#FBF0DF] grayscale-0': route.path === '/',
+            grayscale: route.path !== '/',
+          }"
+        >
+          about 👨‍💻‍</NuxtLink
+        >
+      </span>
 
-            <span class="flex gap-3 sm:flex-row lg:flex-col lg:gap-0">
-                <a class="hover:underline grayscale hover:grayscale-0 hover:text-[color:#FBF0DF] flex-none"
-                    href="https://www.github.com/wajeht" target="_blank">github ↗️</a>
-                <a class="hover:underline grayscale hover:grayscale-0 hover:text-[color:#FBF0DF] flex-none"
-                    href=" https://www.linkedin.com/in/kyawsny/" target="_blank">linkedin ↗️</a>
-                <a class="hover:underline grayscale hover:grayscale-0 hover:text-[color:#FBF0DF] flex-none"
-                    href="https://www.instagram.com/thejaww" target="_blank">instagram ↗️</a>
-            </span>
-        </nav>
-    </div>
+      <span class="flex gap-3 sm:flex-row lg:flex-col lg:gap-0">
+        <a
+          class="hover:underline grayscale hover:grayscale-0 hover:text-[color:#FBF0DF] flex-none"
+          href="https://www.github.com/wajeht"
+          target="_blank"
+          >github ↗️</a
+        >
+        <a
+          class="hover:underline grayscale hover:grayscale-0 hover:text-[color:#FBF0DF] flex-none"
+          href=" https://www.linkedin.com/in/kyawsny/"
+          target="_blank"
+          >linkedin ↗️</a
+        >
+        <ConfirmationLink
+          link="https://instagram.com/thejaww"
+          label="instagram ↗️"
+          content="all thoughts are my own!"
+        />
+      </span>
+    </nav>
+  </div>
 </template>

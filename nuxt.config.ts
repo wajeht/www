2671df86d3ad348f51ae7a43f14f1036163c5787
wajeht <58354193+wajeht@users.noreply.github.com>
2023-03-17 17:50:43 -0500

    fix: zoomout on mobile

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     app: {
         head: {
           charset: 'utf-16',
-          viewport: 'width=500, initial-scale=1',
+          viewport: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, minimal-ui',
           title: 'jaw.dev',
         }
       },

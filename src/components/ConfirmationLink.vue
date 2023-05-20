f5808e5d3bf514521e78c4d00c91f5b60e2fa0e8
wajeht <58354193+wajeht@users.noreply.github.com>
2023-05-20 14:34:29 -0500

    feat: click outside of modal

--- src/components/ConfirmationLink.vue
+++ src/components/ConfirmationLink.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import { OnClickOutside } from '@vueuse/components';
 
 type Props = {
   content: string;
@@ -27,44 +28,48 @@ function go(): void {
 
     <slot @click="showModal = true"></slot>
 
-    <Teleport to="body">
-      <div v-if="showModal" @click="$emit('open')" class="relative z-10" role="dialog">
-        <!-- background -->
-        <div class="fixed inset-0 z-10 flex items-center justify-center p-4 bg-black bg-opacity-75">
-          <!-- content -->
+    <OnClickOutside @trigger="showModal = false">
+      <Teleport to="body">
+        <div v-if="showModal" @click="$emit('open')" class="relative z-10" role="dialog">
+          <!-- background -->
           <div
-            class="flex max-w-3xl flex-col gap-2 px-4 pt-5 pb-4 bg-[#FBFBFB] dark:bg-[#252420] rounded-sm"
+            class="fixed inset-0 z-10 flex items-center justify-center p-4 bg-black bg-opacity-75"
           >
-            <!-- title -->
+            <!-- content -->
             <div
-              class="flex flex-col gap-4 justify-between text-[#1A1A1A] dark:text-[color:#FBF0DF]"
+              class="flex max-w-3xl flex-col gap-2 px-4 pt-5 pb-4 bg-[#FBFBFB] dark:bg-[#252420] rounded-sm"
             >
-              <p>{{ content }}</p>
+              <!-- title -->
+              <div
+                class="flex flex-col gap-4 justify-between text-[#1A1A1A] dark:text-[color:#FBF0DF]"
+              >
+                <p>{{ content }}</p>
 
-              <!-- buttons -->
-              <div class="flex justify-between gap-2">
-                <!-- go ahead -->
-                <button
-                  @click="go()"
-                  class="hover:outline hover:outline-[color:#37362F] flex gap-1 items-center py-1 px-4 rounded-sm font-light hover:bg-[color:#0B0A08] bg-[color:#15140E] text-[color:#D4D3D2] hover:text-[color:#FBF0DF]"
-                >
-                  <small class="text-xs"> ✅ </small>
-                  ok
-                </button>
+                <!-- buttons -->
+                <div class="flex justify-between gap-2">
+                  <!-- go ahead -->
+                  <button
+                    @click="go()"
+                    class="hover:outline hover:outline-[color:#37362F] flex gap-1 items-center py-1 px-4 rounded-sm font-light hover:bg-[color:#0B0A08] bg-[color:#15140E] text-[color:#D4D3D2] hover:text-[color:#FBF0DF]"
+                  >
+                    <small class="text-xs"> ✅ </small>
+                    ok
+                  </button>
 
-                <!-- cancel -->
-                <button
-                  @click="showModal = false"
-                  class="hover:outline hover:outline-[color:#37362F] flex gap-1 items-center py-1 px-4 rounded-sm font-light hover:bg-[color:#0B0A08] bg-[color:#15140E] text-[color:#D4D3D2] hover:text-[color:#FBF0DF]"
-                >
-                  <small class="text-xs"> ❌ </small>
-                  close
-                </button>
+                  <!-- cancel -->
+                  <button
+                    @click="showModal = false"
+                    class="hover:outline hover:outline-[color:#37362F] flex gap-1 items-center py-1 px-4 rounded-sm font-light hover:bg-[color:#0B0A08] bg-[color:#15140E] text-[color:#D4D3D2] hover:text-[color:#FBF0DF]"
+                  >
+                    <small class="text-xs"> ❌ </small>
+                    close
+                  </button>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </Teleport>
+      </Teleport>
+    </OnClickOutside>
   </span>
 </template>
